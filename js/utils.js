8f0a57cabd0cc64dce0fe495502a443d4a100d47
Ilson Warmling <ilsonwarsc@gmail.com>
2023-03-30 12:36:06 -0300

    fix: Correcting punctuation and spacing

--- js/utils.js
+++ js/utils.js
@@ -1,85 +1,87 @@
 // Defindo referências para elementos da página
-var authForm = document.getElementById('authForm')
-var authFormTitle = document.getElementById('authFormTitle')
-var register = document.getElementById('register')
-var access = document.getElementById('access')
+var authForm = document.getElementById("authForm");
+var authFormTitle = document.getElementById("authFormTitle");
+var register = document.getElementById("register");
+var access = document.getElementById("access");
 
-var loading = document.getElementById('loading')
+var loading = document.getElementById("loading");
 
-var auth = document.getElementById('auth')
-var userContent = document.getElementById('userContent')
+var auth = document.getElementById("auth");
+var userContent = document.getElementById("userContent");
 
-var userEmail = document.getElementById('userEmail')
+var userEmail = document.getElementById("userEmail");
 
-var sendEmailVerificationDiv = document.getElementById('sendEmailVerificationDiv')
-var emailVerified = document.getElementById('emailVerified')
+var sendEmailVerificationDiv = document.getElementById(
+  "sendEmailVerificationDiv"
+);
+var emailVerified = document.getElementById("emailVerified");
 
-var passwordReset = document.getElementById('passwordReset')
+var passwordReset = document.getElementById("passwordReset");
 
-var userName = document.getElementById('userName')
-var userImg = document.getElementById('userImg')
-
+var userName = document.getElementById("userName");
+var userImg = document.getElementById("userImg");
 
 // Alterar o formulário de autenticação para o cadastro de novas contas
 function toggleToRegister() {
-  authForm.submitAuthForm.innerHTML = 'Cadastrar conta'
-  authFormTitle.innerHTML = 'Insira seus dados para se cadastrar'
-  hideItem(register) // Esconder atalho para cadastrar conta
-  hideItem(passwordReset) // Esconder a opção de redefinição de senha
-  showItem(access) // Mostrar atalho para acessar conta
+  authForm.submitAuthForm.innerHTML = "Cadastrar conta";
+  authFormTitle.innerHTML = "Insira seus dados para se cadastrar";
+  hideItem(register); // Esconder atalho para cadastrar conta
+  hideItem(passwordReset); // Esconder a opção de redefinição de senha
+  showItem(access); // Mostrar atalho para acessar conta
 }
 
 // Alterar o formulário de autenticação para o acesso de contas já existentes
 function toggleToAccess() {
-  authForm.submitAuthForm.innerHTML = 'Acessar'
-  authFormTitle.innerHTML = 'Acesse a sua conta para continuar'
-  hideItem(access) // Esconder atalho para acessar conta
-  showItem(passwordReset) // Mostrar a opção de redefinição de senha
-  showItem(register) // Mostrar atalho para cadastrar conta
+  authForm.submitAuthForm.innerHTML = "Acessar";
+  authFormTitle.innerHTML = "Acesse a sua conta para continuar";
+  hideItem(access); // Esconder atalho para acessar conta
+  showItem(passwordReset); // Mostrar a opção de redefinição de senha
+  showItem(register); // Mostrar atalho para cadastrar conta
 }
 
 // Simplifica a exibição de elementos da página
 function showItem(element) {
-  element.style.display = 'block'
+  element.style.display = "block";
 }
 
 // Simplifica a remoção de elementos da página
 function hideItem(element) {
-  element.style.display = 'none'
+  element.style.display = "none";
 }
 
 // Mostrar conteúdo para usuários autenticados
 function showUserContent(user) {
-  console.log(user)
-  if (user.providerData[0].providerId != 'password') {
-    emailVerified.innerHTML = 'Autenticação por provedor confiável, não é necessário verificar e-mail'
-    hideItem(sendEmailVerificationDiv)
+  console.log(user);
+  if (user.providerData[0].providerId != "password") {
+    emailVerified.innerHTML =
+      "Autenticação por provedor confiável, não é necessário verificar e-mail";
+    hideItem(sendEmailVerificationDiv);
   } else {
     if (user.emailVerified) {
-      emailVerified.innerHTML = 'E-mail verificado'
-      hideItem(sendEmailVerificationDiv)
+      emailVerified.innerHTML = "E-mail verificado";
+      hideItem(sendEmailVerificationDiv);
     } else {
-      emailVerified.innerHTML = 'E-mail não verificado'
-      showItem(sendEmailVerificationDiv)
+      emailVerified.innerHTML = "E-mail não verificado";
+      showItem(sendEmailVerificationDiv);
     }
   }
-  
-  userImg.src = user.photoURL ? user.photoURL : 'img/unknownUser.png'
-  userName.innerHTML = user.displayName
-  userEmail.innerHTML = user.email
-  hideItem(auth)
-  showItem(userContent)
+
+  userImg.src = user.photoURL ? user.photoURL : "img/unknownUser.png";
+  userName.innerHTML = user.displayName;
+  userEmail.innerHTML = user.email;
+  hideItem(auth);
+  showItem(userContent);
 }
 
 // Mostrar conteúdo para usuários não autenticados
 function showAuth() {
-  authForm.email.value = ''
-  authForm.password.value = ''
-  hideItem(userContent)
-  showItem(auth)
+  authForm.email.value = "";
+  authForm.password.value = "";
+  hideItem(userContent);
+  showItem(auth);
 }
 
 // Atributos extras de configuração de e-mail
 var actionCodeSettings = {
-  url: 'http://127.0.0.1:5500/'
-}
+  url: "http://127.0.0.1:5500/",
+};
